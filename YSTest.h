2d7a6a96e02585190add421bb65d1d3a0e58a63c
Yuriy <sheff_ne@yahoo.com>
2014-10-08 21:28:19 -0500

    added test class

---
+++ YSTest.h
@@ -0,0 +1,13 @@
+//
+//  YSTest.h
+//  gitTest
+//
+//  Created by Yuriy Shefer on 10/8/14.
+//  Copyright (c) 2014 SHEFFcode. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YSTest : NSObject
+
+@end
---
+++ YSTest.m
@@ -0,0 +1,13 @@
+//
+//  YSTest.m
+//  gitTest
+//
+//  Created by Yuriy Shefer on 10/8/14.
+//  Copyright (c) 2014 SHEFFcode. All rights reserved.
+//
+
+#import "YSTest.h"
+
+@implementation YSTest
+
+@end
