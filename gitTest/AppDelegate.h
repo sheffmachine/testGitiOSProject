97a3b704828fc9892571ba289d35b96a7b032d98
Yuriy <sheff_ne@yahoo.com>
2014-10-08 21:06:48 -0500

    Initial Commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by Yuriy Shefer on 10/8/14.
+//  Copyright (c) 2014 SHEFFcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by Yuriy Shefer on 10/8/14.
+//  Copyright (c) 2014 SHEFFcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
